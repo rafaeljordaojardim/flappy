5a0d7d2b68d5db3dbb7a887dace590d67656bbb0
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2019-07-01 11:57:53 -0300

    tentando colocar touchstart e touchend

--- js/flappy.js
+++ js/flappy.js
@@ -85,7 +85,8 @@ function Passaro(alturaJogo) {
 
     window.onkeydown = e => voando = true
     window.onkeyup = e => voando = false
-
+    window.ontouchstart = e => voando = true
+    window.ontouchend = e => voando = false
     this.animar = () => {
         const novoY = this.getY() + (voando ? 8 : -5)
         const alturaMaxima = alturaJogo - this.elemento.clientWidth
